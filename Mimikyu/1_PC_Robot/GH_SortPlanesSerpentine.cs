5cc673f7124be789b8e9ccfb4ed8b94d7b9b7d02
You-Wei Yen <youwei@chalmers.se>
2026-09-03 10:38:06 +0200

    serpentine directions x or y

--- Mimikyu/1_PC_Robot/GH_SortPlanesSerpentine.cs
+++ Mimikyu/1_PC_Robot/GH_SortPlanesSerpentine.cs
@@ -26,7 +26,9 @@ namespace Mimikyu._1_PC_Robot
         {
             pManager.AddPlaneParameter("Planes", "P", "Unordered Planes", GH_ParamAccess.list);
             pManager.AddPointParameter("CameraBase", "CB", "Height where camera should flip to the other side", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("SortBy", "S", "Sort by 0: X, 1: Y", GH_ParamAccess.item, 1);
             pManager[1].Optional = true;
+            pManager[2].Optional = true;
         }
 
         /// <summary>
@@ -45,41 +47,75 @@ namespace Mimikyu._1_PC_Robot
         {
             List<Plane> planes = new List<Plane>();
             Point3d cameraBase = Point3d.Unset;
+            int sortBy = 1;
 
             if (!DA.GetDataList(0, planes)) return;
             DA.GetData(1, ref cameraBase);
+            DA.GetData(2, ref sortBy);
+
 
             double rowTolerance = 10.0;
-
-            // Sort by Y first
-            var sortedByY = planes
-                .OrderBy(p => p.OriginY)
-                .ToList();
-
+            List<Plane> sortedBy = new List<Plane>();
             // Group into rows
             List<List<Plane>> rows = new List<List<Plane>>();
             List<Plane> currentRow = new List<Plane>();
-
-            double currentY = sortedByY[0].OriginY;
-
-            foreach (var p in sortedByY)
+            // Sort by Y first
+            switch (sortBy)
             {
-                if (Math.Abs(p.OriginY - currentY) <= rowTolerance)
-                {
-                    currentRow.Add(p);
-                }
-                else
-                {
+                case 1: 
+                     sortedBy = planes
+                        .OrderBy(p => p.OriginY)
+                        .ToList();
+
+                    double currentY = sortedBy[0].OriginY;
+
+                    foreach (var p in sortedBy)
+                    {
+                        if (Math.Abs(p.OriginY - currentY) <= rowTolerance)
+                        {
+                            currentRow.Add(p);
+                        }
+                        else
+                        {
+                            rows.Add(currentRow);
+
+                            currentRow = new List<Plane>();
+                            currentRow.Add(p);
+
+                            currentY = p.OriginY;
+                        }
+                    }
+
                     rows.Add(currentRow);
 
-                    currentRow = new List<Plane>();
-                    currentRow.Add(p);
+                    break;
+                case 0:
+                    sortedBy = planes
+                        .OrderBy(p => p.OriginX)
+                        .ToList();
 
-                    currentY = p.OriginY;
-                }
+                    double currentX = sortedBy[0].OriginX;
+
+                    foreach (var p in sortedBy)
+                    {
+                        if (Math.Abs(p.OriginX - currentX) <= rowTolerance)
+                        {
+                            currentRow.Add(p);
+                        }
+                        else
+                        {
+                            rows.Add(currentRow);
+
+                            currentRow = new List<Plane>();
+                            currentRow.Add(p);
+
+                            currentX = p.OriginX;
+                        }
+                    }
+
+                    rows.Add(currentRow);
+                    break;
             }
-
-            rows.Add(currentRow);
 
             // Build serpentine path
             List<Plane> serpentinePlanes = new List<Plane>();
